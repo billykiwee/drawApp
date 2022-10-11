7aef2218c1cf9184354d600da851232c2b528c8f
Billy <billyturpin642@gmail.com>
2022-10-11 15:52:02 +0200

    button control

--- src/Camera.js
+++ src/Camera.js
@@ -18,10 +18,8 @@ export default function Camera() {
     function cameraStart() {
 
         // Define constants
-        const cameraView    = document.querySelector("#camera--view")
-        
-        navigator.mediaDevices
-        .getUserMedia(constraints)
+        const cameraView = document.querySelector("#camera--view")
+        navigator.mediaDevices.getUserMedia(constraints)
         .then(stream => {
             cameraView.srcObject = stream
         })
@@ -38,8 +36,9 @@ export default function Camera() {
 
     const [Opacity, setOpacity] = useState(70)
     const [Position, setPosition] = useState(0)
-    const [Zoom, setZoom] = useState(88)
-    const widthMin = 50
+    const [Zoom, setZoom] = useState(100)
+    const widthMin = 100
+    const widthStep = 20
 
     const [LoadImag, setLoadImg] = useState('')
 
@@ -56,10 +55,7 @@ export default function Camera() {
         }
 
     }
-/* 
-    useEffect(e=> {
-        DragImage(document.getElementById('Drag_Image'), 'reset')    
-    }, [LoadImag]) */
+
 
 
 
@@ -69,19 +65,24 @@ export default function Camera() {
 
             <div className="grid h-100vh overflow-hidden">
 
-                <div className="grid justify-c">
+                <div className="display justify-c">
 
                     <div className="w-100 h-100 grid absolute overflow-hidden zi-2">
 
                         {
                             LoadImag&&
                             <div className='display justify-c w-100 gap zi-2' style={{height: '10vh'}} >
-                                <ButtonControl onClick={e=> setZoom(Zoom >= widthMin ? Zoom -10 : Zoom)} >
-                                    <svg width={22} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM13.5 10.5h-6" /></svg>
+                                <ButtonControl onClick={e=> setPosition(Position -90)} >
+                                    <svg width={22} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" /></svg>
                                 </ButtonControl>
-                                <ButtonControl onClick={e=> setZoom(Zoom +10)}>
-                                    <svg width={22} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6" /></svg>
-                                </ButtonControl>
+                                <div className='display white border-r-1'>
+                                    <ButtonControl onClick={e=> setZoom(Zoom !== 100 ? Zoom - widthStep : Zoom)} >
+                                        <svg width={22} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM13.5 10.5h-6" /></svg>
+                                    </ButtonControl>
+                                    <ButtonControl onClick={e=> setZoom(Zoom +widthStep)}>
+                                        <svg width={22} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6" /></svg>
+                                    </ButtonControl>
+                                </div>
                             </div>
                         }
 
@@ -92,7 +93,6 @@ export default function Camera() {
                                         src={LoadImag} 
                                         Opacity={Opacity} 
                                         Position={Position} 
-                                        Zoom={Zoom} 
                                     />
                                 </div>
                             </Draggable>
@@ -105,9 +105,6 @@ export default function Camera() {
                                     <input onChange={e=> previewFile(e)} type='file' accept="image/png, image/jpeg"  className='no-click' hidden id='upload' />
                                 </label>
                             </ButtonControl>
-                            <ButtonControl onClick={e=> setPosition(Position -90)} >
-                                <svg width={22} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" /></svg>
-                            </ButtonControl>
                             <ButtonControl >
                                 <svg width={22} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">  <path strokeLinecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" /></svg>
                             </ButtonControl>
@@ -119,7 +116,7 @@ export default function Camera() {
                     </div>
 
                     {/* <canvas id="camera--sensor" className="absolute h-100"></canvas> */}
-                    <video controls={false} id="camera--view" autoPlay playsInline className='w-100 h-100vh fit-cover' /> 
+                    <video  style={{transform: `scale(${Zoom/100})`}}  controls={false} id="camera--view" autoPlay playsInline className='transition w-100 h-100vh fit-cover' /> 
                 </div>
 
             </div>
@@ -137,7 +134,6 @@ function Image({src, Opacity, Position, Zoom}) {
             style={{
                 opacity: Opacity / 100,
                 transform: `rotate(${Position}deg)`,
-                width: `${Zoom}%`
             }} 
             src={src} 
             className="click w-100 transition" 
@@ -150,7 +146,7 @@ function ButtonControl({children, onClick}) {
 
     return (
         <div className='display'>
-            <button type='button' className='click grey h-3 w-3 hover-white' onClick={onClick}>
+            <button type='button' className='click white h-3 w-3 hover-white' onClick={onClick}>
                 <span className='display'>
                     {children}
                 </span>
